717520511a32fc276137abb526a2c670f09e43e9
Ashish Chandwani <Ashish.s.chandwani@gmail.com>
2013-02-05 00:50:53 -0800

    added ghetto php processor

---
+++ calculate.php
@@ -0,0 +1,52 @@
+<?
+  $savings = $_GET["savings"];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Solar Calculator</title>
+	<script type="text/javascript" src="js/jqmin.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css">
+	<link rel="stylesheet" type="text/css" href="calculator.css">
+</head>
+<body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#">Custom Solar</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li><a href="index.html">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+              <li class="active"><a href="calculator.html">Solar Calculator</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+    	<h1>Solar Calculator</h1>
+    	<fieldset>
+      <form action="calculate.php" method="get">
+        <label>How much of your electric bill would you like to eliminate on a monthly basis:<br>
+          $<input type="number" name="savings">
+          <input type="submit" value="Calculate">
+        </label>
+        <table>
+          <th><?= $_GET["savings"]?></th>
+        </table>
+      </form>
+    </fieldset>
+
+    </div> <!-- /container -->
+</body>
+</html>

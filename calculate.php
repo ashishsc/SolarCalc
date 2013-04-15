461b395b6049ce873338c26430c70e7922093a92
ashishsc <ashish.s.chandwani@gmail.com>
2013-04-14 17:43:54 -0700

    Added new js file for highlighting, added tables
    Added table injection via php, green highlighting on paid back row,
    addditonal functions

--- calculate.php
+++ calculate.php
@@ -1,10 +1,16 @@
 <?php
   define("KWH_FIXED", 0.14);
-  define("SYS_DIVISOR", 1440);
+  define("SYS_DIVISOR", 1440); // Used version
   define("WATTS_PER_PANEL", 260);
+  define("ANNUAL_INFLATION_RATE", 8.25); // percent
+  define("COST_PER_WATT", 3.75);
+  define("SSR_REBATE", 1);
+  define('TAX_CREDIT', 1);
+  define('REC_CREDIT', 1);
+
+
 
   $monthly_savings = $_GET["savings"];
-  calculate($monthly_savings);
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,6 +22,7 @@
 	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css">
 	<link rel="stylesheet" type="text/css" href="calculator.css">
+  <script type="text/javascript" src="js/calculator.js"></script>
 </head>
 <body>
     <div class="navbar navbar-inverse navbar-fixed-top">
@@ -47,14 +54,30 @@
           <input type="number" name="savings">
           <input type="submit" value="Calculate">
         </label>
+
       </form>
     </fieldset>
+    <?php
+    calculate($monthly_savings);
+    ?>
+      <table id="payback">
+        <tr>
+          <th>Year</th>
+          <th>KwH Cost(with <?= constant("ANNUAL_INFLATION_RATE") ?>% inflation)</th>
+          <th>Savings per month</th>
+          <th>Savings per year</th>
+          <th>REC payment per month</th>
+          <th>Total Savings</th>
+        </tr>
+      </table>
 
     </div> <!-- /container -->
 </body>
 </html>
 
 <?php
+
+// Do the base calculations for the table
 function calculate($monthly_savings) {
   // Get constants
   $kwh_fixed = constant("KWH_FIXED");
@@ -67,7 +90,65 @@ function calculate($monthly_savings) {
   $system_size = $kw_yearly / $system_size_constant / 100;
   $total_watts = $system_size * 1000;
   $num_panels = ceil($total_watts / $watts_per_panel); // round up for panels
-  
+  addCalculationsTable($system_size, $kw_per_month, $kw_yearly);
+}
 
+
+// Add the calculated info table
+function addCalculationsTable($system_size, $kw_per_month, $kw_yearly) {
+  ?>
+  <table id="calculations">
+    <!-- Section 1 -->
+    <tr><th colspan="3">System Size and Production</th></tr>
+    <tr><td>System Size</td>         <td>KwH per Month produced</td> <td>KwH Annual produced</td></tr>
+    <tr><td><?= $system_size ?></td> <td><?= $kw_per_month ?></td>   <td><?= $kw_yearly ?></td></tr>
+    <tr><td>Number of Panels</td><td><?= $num_panels ?></td></tr>
+
+
+
+
+    <!-- Section 2 -->
+    <tr><th colspan="2">Rebates</th></tr>
+    <tr><td>Solar Rewards Rebate (current Xcel Rebate)</td>         <td>$</td></tr>
+    <tr><td>REC Payment (Current Xcel - paid monthly to owner)</td> <td>$/month</td></tr>
+    <tr><td>Federal Energy Tax Credit</td>                          <td>$</td></tr>
+
+    <tr><th colspan="2">System</th></tr>
+    <tr><td>Gross Cost of System</td>                     <td>$</td></tr>
+    <tr><td>Solar Rewards Rebate</td>                     <td>$</td></tr>
+    <tr><td>After Rebate Cost</td>                        <td>$</td></tr>
+    <tr><td>Federal Tax Credit (30% of gross)</td>        <td>$</td></tr>
+    <tr><td>Subtotal</td>                                 <td>$</td></tr>
+    <tr><td>REC credit (10-20 years -- paid monthly)</td> <td>$</td></tr>
+    <tr><td>Net Cost of Solar System</td>                 <td>$</td></tr>
+  </table>
+  <?php
+}
+
+// Adds the payback rows to the output table
+function addPaybackTable() {
+
+}
+
+
+// adds a row to the table, if the system is fully paid off, then that row will be marked with
+// the balance-point ID
+function addRow($year, $kwh_cost, $savings_per_month, $savings_per_year, $rec_per_month, $total_savings, $subtotal) {
+  ?> 
+  <tr <?php balanceMet($total_savings, $subtotal); ?>>
+    <td><?= $year ?></td>
+    <td><?= $kwh_cost ?></td>
+    <td><?= $savings_per_month ?></td>
+    <td><?= $savings_per_year ?></td>
+    <td><?= $total_savings ?></td>
+  </tr>
+  <?php
+}
+
+// adds the special id if the system is all paid back
+function balanceMet($total_savings, $subtotal) {
+  if ($total_savings >= $subtotal) {
+    echo('id="balance-point"');
+  }
 }
 ?>
---
+++ js/calculator.js
@@ -0,0 +1,8 @@
+// Ashish Chandwani 
+// Checks for the paid-back turning point and adds green 
+'use strict'
+
+$(document).ready(function() {
+	$('#balance-point').addClass('paid-back');
+	$('#calculations').show();
+});
